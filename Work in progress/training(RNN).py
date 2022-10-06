a900e031d18235e5d0b26146d9f0ae0ccadd05ee
OnceByGee <maurizio.demarchi@studenti.unitn.it>
2022-10-06 18:49:43 +0200

    wip

--- Work in progress/training(RNN).py
+++ Work in progress/training(RNN).py
@@ -9,37 +9,72 @@ from RNNs import *
 
 argv = sys.argv[1:]
 csv_file = ''
+
+batch_size = 128
+hidden_dim = 64
+layer_dim = 3
+dropout = 0.2
+n_epochs = 100
+learning_rate = 1e-3
+weight_decay = 1e-6
+neural_network_kind = "LSTM"
+
 try:
-    opts, args = getopt.getopt(argv, "hi", ["ifile="])
+    opts, args = getopt.getopt(argv, "hni:bHLelwd", ["network=", "input-file=", "batch=", "hidden-dim=", "layer-dim=",
+                                                     "epochs=", "dropout=", "learning-rate=", "weight-decay"])
 except getopt.GetoptError:
     print("training(RNN).py -i csv_file_path")
     sys.exit(2)
 
 for opt, arg in opts:
-    if opt == '-h':
-        print('training(RNN).py -i csv_file_path')
+    if opt in ('-h', '--help'):
+        print(f"""training(RNN).py -i csv_file_path
+        Arguments: 
+         -h, --help: elenca gli argomenti necessari
+         -n, --neural-network: uno tra RNN, LSTM e GRU (default: {neural_network_kind})
+         -i, --input-file: percorso del file csv da passare come input
+         -b, --batch: batch per il training (default: {batch_size})
+         -H, --hidden-dim: numero di hidden layers (default: {hidden_dim})
+         -L, --layer-dim: dimensione dei layer (default: {layer_dim})
+         -e, --epochs: epoche per cui addestrare la rete (default: {n_epochs})
+         -l, --learning-rate: rate di apprendimento della rete (default: {learning_rate})
+         -w, --weight-decay: decadimento dei pesi (default: {weight_decay})
+         -d, --dropout: dropout dei neuroni (default: {dropout})""")
         sys.exit(0)
     elif opt in ("-i", "--ifile"):
         csv_file = arg
+    elif opt in ("-n", "--neural-network"):
+        neural_network_kind = arg
+    elif opt in ("-b", "--batch"):
+        batch_size = int(arg)
+    elif opt in ("-H", "--hidden-dim"):
+        hidden_dim = int(arg)
+    elif opt in ("-L", "--layer-dim"):
+        layer_dim = int(arg)
+    elif opt in ("-e", "--epochs"):
+        n_epochs = int(arg)
+    elif opt in ("-l", "--learning-rate"):
+        learning_rate = float(arg)
+    elif opt in ("-w", "--weight-decay"):
+        weight_decay = float(arg)
+    elif opt in ("-d", "--dropout"):
+        dropout = float(arg)
 
 converter = importer.CellCsvConverter()
-train_dataset, test_dataset = converter.Convert_csv_to_Dataset(csv_file, train_percentage=0.7, validation=True)
+train_dataset, validation_dataset, test_dataset = converter.Convert_csv_to_Dataset(csv_file,
+                                                                                   train_percentage=0.7,
+                                                                                   validation_percentage=0.2)
+
 
 # data loaders
-train_loader = DataLoader(train, batch_size=batch_size, shuffle=False, drop_last=True)
-val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, drop_last=True)
-test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, drop_last=True)
-test_loader_one = DataLoader(test, batch_size=1, shuffle=False, drop_last=True)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+val_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+test_loader_one = DataLoader(test_dataset, batch_size=1, shuffle=False, drop_last=True)
 
-input_dim = len(X_train.columns)
+input_dim = len(train_dataset)
 output_dim = 1
-hidden_dim = 64
-layer_dim = 3
-batch_size = 64
-dropout = 0.2
-n_epochs = 100
-learning_rate = 1e-3
-weight_decay = 1e-6
+
 
 model_params = {'input_dim': input_dim,
                 'hidden_dim': hidden_dim,

8a044fe8e4467000ef2f3ef0666ee34c809f13c8
matteodv99tn <matteodv99tn@gmail.com>
2022-10-14 10:15:23 +0200

    Dati Ipogeo

--- Codice/constants.py
+++ Codice/constants.py
@@ -3,4 +3,4 @@ import os
 SOURCE_DIR    = os.path.realpath(os.path.dirname(__file__))
 ROOT_DIR      = SOURCE_DIR[:SOURCE_DIR.rfind(os.sep)]
 PURE_DATA_DIR = os.path.join(ROOT_DIR, 'Dati originali')
-DATA_DIR      = os.path.join(ROOT_DIR, 'Dati')
+DATA_DIR      = os.path.join(ROOT_DIR, 'CSV')

488b5398d53decc0dcad2c85a0f3dfb3ad05670c
matteodv99tn <matteodv99tn@gmail.com>
2022-10-16 09:54:23 +0200

    Update import_dataframe.py

--- Codice/import_dataframe.py
+++ Codice/import_dataframe.py
@@ -1,6 +1,7 @@
 import  pandas      as pd
 import  os.path
 from    constants   import DATA_DIR
+from    collections import defaultdict
 
 def get_cell_list() -> tuple:
     ''' Ritorna il nome di tutti i file presenti nella cartella "CSV" '''
@@ -28,17 +29,22 @@ def import_single_csv(filename: str) -> pd.DataFrame:
         new_col_names.append( df.columns[i][len(filename)-4:])
     df.columns = new_col_names
     # Converto eventuali valori stringhe in floats
-    df = df.applymap(str_to_float)
+    df = df.applymap(str_to_float, )
     df.dropna()
     return df
 
-def import_all_csv() -> dict:
-    ''' Crea un dizionario con tutti i file csv '''
-    res = {}
+def import_all_csv() -> defaultdict:
+    ''' Crea un dizionario con tutti i file csv 
+    L'idea è che dopo è possibile chiamare i dataframe di ogni singola cella 
+    come `res['nome_lotto']['nome_cella']`, 
+    per esempio `res['Ipogeo2']['Cella13']`.
+    '''
+    res = defaultdict(dict)
     for filename in get_cell_list():
+        lbl_lot  = filename[:filename.find('_')]
+        lbl_cell = filename[filename.find('_')+1:filename.find('.')]
         print(f'Importing file "{filename}"')
-        lbl = filename[filename.find('_')+1:filename.find('.')]
-        res[lbl] = import_single_csv(filename)
+        res[lbl_lot][lbl_cell] = import_single_csv(filename)
     
     return res
 

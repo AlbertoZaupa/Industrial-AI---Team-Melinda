f9923b27ee93b36014d8848b835294e28981243c
Alberto Zaupa <alberto.zaupa@studenti.unitn.it>
2022-11-16 10:16:55 +0100

    Supporto per la classificazione per il modello encoder-decoder

--- Codice/RNNs(Keras)/encoder-decoder/dataset.py
+++ Codice/RNNs(Keras)/encoder-decoder/dataset.py
@@ -1,23 +1,61 @@
 import tensorflow as tf
 
 
-def prepare_dataset(df, n_input_features, past_window_size, forecast_size, batch_size, shuffle=False):
+def prepare_dataset_classification(df1, df2, past_window_size, forecast_window_size, n_input_features,
+                                   batch_size, vectorized_labels=False):
+    """
+        df1: dataframe di input, tranne la variabile da classificare
+        df2: dataframe contenente solo la variabile da classificare
+        n_input_features: il numero di feature che rappresentano l'input al sistema (TemperaturaMandataGlicole)
+        past_window_size: il numero di minuti per cui si guarda nel passato
+        forecast_size: il numero di minuti per cui si prevede nel futuro
+        batch_size: dimensione di una batch durante la fase di training
+        vectorized_labels: se True le labels sono un vettore di lunghezza <forecast_window_size>, altrimenti le labels
+            sono uno scalare corrispondente al valore della variabile al termine della finestra di predizione
+      """
+
+    total_size = past_window_size + forecast_window_size
+    dataset2 = tf.one_hot(df2.values.astype('uint8'), 2)
+    data = tf.concat([dataset2, df1.values.astype('float32')], axis=1)
+    data = tf.data.Dataset.from_tensor_slices(data)
+    data = data.window(total_size, shift=1, drop_remainder=True)
+    data = data.flat_map(lambda k: k.batch(total_size))
+
+    return split_inputs_and_outputs(data, forecast_window_size, n_input_features, batch_size, vectorized_labels)
+
+
+def prepare_dataset_regression(df, n_input_features, past_window_size, forecast_window_size, batch_size,
+                               vectorized_labels=False):
     """
       df: dataframe di input
       n_input_features: il numero di feature che rappresentano l'input al sistema (TemperaturaMandataGlicole)
       past_window_size: il numero di minuti per cui si guarda nel passato
       forecast_size: il numero di minuti per cui si prevede nel futuro
       batch_size: dimensione di una batch durante la fase di training
+      vectorized_labels: se True le labels sono un vettore di lunghezza <forecast_window_size>, altrimenti le labels
+          sono uno scalare corrispondente al valore della variabile al termine della finestra di predizione
     """
 
-    total_size = past_window_size + forecast_size
+    total_size = past_window_size + forecast_window_size
     data = tf.data.Dataset.from_tensor_slices(df.values.astype('float32'))
     data = data.window(total_size, shift=1, drop_remainder=True)
     data = data.flat_map(lambda k: k.batch(total_size))
 
-    if shuffle:
-        shuffle_buffer_size = len(df)
-        data = data.shuffle(shuffle_buffer_size)
+    return split_inputs_and_outputs(data, forecast_window_size, n_input_features, batch_size, vectorized_labels)
+
+
+def split_inputs_and_outputs(dataset, forecast_window_size, n_input_features, batch_size, vectorized_labels=False):
+    # Per la feature che cerchiamo di predirre, possiamo scegliere di ottenere come
+    # label il vettore di valori futuri dal momento corrente
+    # al termine di <forecast_size>, oppure solo l'ultimo elemento del vettore.
+
+    labels_range = forecast_window_size if vectorized_labels else 1
+
+    def future_inputs_lambda(k):
+        if vectorized_labels:
+            return k[-forecast_window_size:, -n_input_features:]
+        else:
+            return tf.transpose(k[-forecast_window_size:, -n_input_features:])
 
     # k[:-forecast_size] è la matrice che contiene il valore delle variabili di stato
     # e delle variabili di input nei <past_window_size> minuti precedenti
@@ -28,8 +66,8 @@ def prepare_dataset(df, n_input_features, past_window_size, forecast_size, batch
     # k[-forecast_size:, 0] è il valore della variabile che vogliamo predirre
     # nei <forecast_size> minuti successivi
 
-    data = data.map(lambda k: ((k[:-forecast_size],
-                                tf.transpose(k[-forecast_size:, -n_input_features:])),
-                               k[-1:, 0]))
+    dataset = dataset.map(lambda k: ((k[:-forecast_window_size],
+                                      future_inputs_lambda(k)),
+                                     k[-labels_range:, 0]))
 
-    return data.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    return dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
--- Codice/RNNs(Keras)/encoder-decoder/model.py
+++ Codice/RNNs(Keras)/encoder-decoder/model.py
@@ -4,7 +4,8 @@ LSTM_UNITS = 128
 
 
 # Un modello basato sull'architettura encoder-decoder
-def encoder_decoder_arch(past_window_len, n_state_features, future_window_len, n_input_features):
+def encoder_decoder_arch(past_window_len, n_state_features, future_window_len, n_input_features,
+                         regression=True, whole_output_sequence=False):
     global LSTM_UNITS
 
     # La parte della rete che ha il ruolo di encoder è una LSTM
@@ -15,16 +16,34 @@ def encoder_decoder_arch(past_window_len, n_state_features, future_window_len, n
     # dello 'stato' o 'contesto' del sistema che è stata prodotta dall'encoder, insieme
     # ai valori futuri della temperatura del glicole
 
-    future_inputs = tf.keras.Input(shape=(n_input_features, future_window_len), name='future_inputs')
+    # Possiamo richiedere alla rete di prevedere l'intera sequenza di valori futuri nella finestra
+    # temporale scelta, oppure di prevedere solo il valore al termine della finestra
+    future_inputs_shape = (future_window_len, n_input_features) if whole_output_sequence else (n_input_features, future_window_len)
+    future_inputs = tf.keras.Input(shape=future_inputs_shape, name='future_inputs')
     decoder_lstm = tf.keras.layers.LSTM(LSTM_UNITS, return_sequences=True)(future_inputs, initial_state=[state_h, state_c])
 
     # Dei layer lineari vengono aggiunti al decoder
     x = tf.keras.layers.Dense(32, activation='relu')(decoder_lstm)
     x = tf.keras.layers.Dense(16, activation='relu')(x)
-    output = tf.keras.layers.Dense(1, activation='linear')(x)
+    finalize_model = finalize_model_regression if regression else finalize_model_classification
+    return finalize_model(past_inputs, future_inputs, x)
+
+
+def finalize_model_regression(past_inputs, future_inputs, second_to_last_layer):
+    output = tf.keras.layers.Dense(1, activation='linear')(second_to_last_layer)
 
     # Il modello viene compilato
-    model = tf.keras.models.Model(inputs=[past_inputs,future_inputs], outputs=output)
+    model = tf.keras.models.Model(inputs=[past_inputs, future_inputs], outputs=output)
     model.compile(loss='mse', optimizer='adam', metrics=["mse"])
+
+    return model
+
+
+def finalize_model_classification(past_inputs, future_inputs, second_to_last_layer):
+    output = tf.keras.layers.Dense(2, activation='softmax')(second_to_last_layer)
+
+    # Il modello viene compilato
+    model = tf.keras.models.Model(inputs=[past_inputs, future_inputs], outputs=output)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
 
     return model

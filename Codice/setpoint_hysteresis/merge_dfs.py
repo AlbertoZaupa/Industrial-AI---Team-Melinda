38e4e0d5b7ae47fcf1ce80de6a5b0d2028a41dac
Alberto Zaupa <alberto.zaupa@studenti.unitn.it>
2022-11-16 16:27:53 +0100

    modificato strategia di estrazione setpoint ed isteresi. ora guardo a quando si accende la pompa del glicole

--- Codice/setpoint_hysteresis/merge_dfs.py
+++ Codice/setpoint_hysteresis/merge_dfs.py
@@ -6,10 +6,9 @@ OUTPUT_PATH = "/Users/albertozaupa/Desktop/Projects/Melinda/datasets/Cella_13_SH
 
 if __name__ == "__main__":
     cell13_df = pd.read_csv(DATASET_PATH)
-    sh_values = pd.read_csv(SH_PATH)
-    sh_values = sh_values.transpose().values
-    cell13_df["Setpoint"] = sh_values[:-1, :1]
-    cell13_df["Isteresi"] = sh_values[:-1, 1:]
-    print(cell13_df.head())
+    sh_values = pd.read_csv(SH_PATH)[["0", "1"]]
+    sh_values = sh_values.values
+    cell13_df["Setpoint"] = sh_values[:, :1]
+    cell13_df["Isteresi"] = sh_values[:, 1:]
     cell13_df.to_csv(OUTPUT_PATH)
 

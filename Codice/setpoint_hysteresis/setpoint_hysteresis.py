5eb8929d5f97c07a096221c12ab75532b52a028f
Alberto Zaupa <alberto.zaupa@studenti.unitn.it>
2022-11-17 21:52:59 +0100

    script per creare i dataset per la rete usata quando la cella è spenta, e quando è accesa

--- Codice/setpoint_hysteresis/setpoint_hysteresis.py
+++ Codice/setpoint_hysteresis/setpoint_hysteresis.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-DATASET_PATH = "/Users/albertozaupa/Desktop/Projects/Melinda/datasets/Cella_13.csv"
-OUTPUT_PATH = "/Users/albertozaupa/Desktop/Projects/Melinda/datasets/Cella_13_SH.csv"
-TEMP_COLUMN = "TemperaturaCelle"
-PUMP_COLUMN = "PompaGlicoleMarcia"
+DATASET_PATH = "/Users/albertozaupa/Desktop/Projects/Melinda/datasets/Cella_17.csv"
+OUTPUT_SH = "/Users/albertozaupa/Desktop/Projects/Melinda/datasets/Cella_17_SH.csv"
+OUTPUT_NN_OFF = "/Users/albertozaupa/Desktop/Projects/Melinda/datasets/Cella_17_NN_off.csv"
+OUTPUT_NN_ON = "/Users/albertozaupa/Desktop/Projects/Melinda/datasets/Cella_17_NN_on.csv"
+TEMP_COLUMN = "Ipogeo2_Cella17TemperaturaCelle"
+PUMP_COLUMN = "Ipogeo2_Cella17PompaGlicoleMarcia"
+
+off_slices = []
+on_slices = []
 
 
 def get_setpoint_hysteresis(pump, temp, n):
@@ -16,18 +21,31 @@ def get_setpoint_hysteresis(pump, temp, n):
 
     looking_for_1 = True
 
+    off_start = 0
+    on_start = None
+
     for i in range(window_size, n):
         if looking_for_1 and pump[i] == 1:
             window = temp[i - window_size : min(i + window_size + 1, n)]
             window_argmax = window.argmax() + i - window_size
             window_max = temp[window_argmax]
+
+            on_start = window_argmax
+            off_slices.append((off_start, window_argmax + 1))
+            off_start = None
             output_hysteresis.append((window_argmax, window_max))
+
             looking_for_1 = False
         elif not looking_for_1 and pump[i] == 0:
             window = temp[i - window_size: min(i + window_size + 1, n)]
             window_argmin = window.argmin() + i - window_size
             window_min = temp[window_argmin]
+
+            off_start = window_argmin
+            on_slices.append((on_start, window_argmin + 1))
+            on_start = None
             output_setpoint.append((window_argmin, window_min))
+
             looking_for_1 = True
 
     return output_setpoint, output_hysteresis
@@ -57,15 +75,45 @@ def extend(inputs, n):
     return output
 
 
+def create_dataset_from_slices(source_dataset, slices_list):
+    numpy_dataset = None
+
+    for el in slices_list:
+        start = el[0]
+        end = el[1]
+        new_slice = source_dataset[start:end].copy()
+
+        if numpy_dataset is None:
+            numpy_dataset = new_slice
+        else:
+            numpy_dataset = np.concatenate((numpy_dataset, new_slice), axis=0)
+
+    return numpy_dataset
+
+
 if __name__ == "__main__":
     df = pd.read_csv(DATASET_PATH)
     temp = df[TEMP_COLUMN]
-    temp.replace({',': '.'}, regex=True).astype(float)
+    temp = temp.replace({',': '.'}, regex=True).astype(float)
     pump = df[PUMP_COLUMN]
 
+    # Vengono ricavati i valori di setpoint ed isteresi
     setpoints, hysteresises = get_setpoint_hysteresis(pump, temp, df.shape[0])
     final_setpoints = extend(setpoints, df.shape[0])
     final_hysteresises = extend(hysteresises, df.shape[0])
+
+    # Vengono creati i due dataset separati
+    off_dataset = create_dataset_from_slices(temp.values, off_slices)
+    plt.plot(off_dataset)
+    plt.show()
+    on_dataset = create_dataset_from_slices(temp.values, on_slices)
+    plt.plot(on_dataset)
+    plt.show()
+
+    if input("Satisfied?" == "Y"):
+        pd.DataFrame(np.transpose(np.array([final_setpoints, final_hysteresises]))).to_csv(OUTPUT_SH)
+        pd.DataFrame(off_dataset).to_csv(OUTPUT_NN_OFF)
+        pd.DataFrame(on_dataset).to_csv(OUTPUT_NN_ON)
 
     # display_setpoints = np.zeros((df.shape[0]), )
     # display_hysteresises = np.zeros((df.shape[0]), )
@@ -101,11 +149,6 @@ if __name__ == "__main__":
     #     plt.show()
     #     input()
 
-    plt.plot(final_setpoints, label="setpoint")
-    plt.plot(final_hysteresises, label="hysteresis")
-    plt.legend()
-    plt.show()
-
-    if input("Satisfied? ") == "Y":
-        new_df = pd.DataFrame(np.transpose(np.array([final_setpoints, final_hysteresises])))
-        new_df.to_csv(OUTPUT_PATH)
+    # if input("Satisfied? ") == "Y":
+    #     new_df = pd.DataFrame(np.transpose(np.array([final_setpoints, final_hysteresises])))
+    #     new_df.to_csv(OUTPUT_PATH)
